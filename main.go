4726e64b2407dae1ba6c3f617655f32084525861
irfn <mail.urfi@gmail.com>
2018-04-23 17:04:41 +0530

    change main contents again

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there from hello-web-1-%s, %s!\n", os.Getenv("HELLO_ENV"), r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there from hello-web-2-%s, %s!\n", os.Getenv("HELLO_ENV"), r.URL.Path[1:])
 }
 
 func main() {
